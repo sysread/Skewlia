a9c96e4d1964ec54e81d7068750a6ae1f43bec63
Documenter.jl <documenter@juliadocs.github.io>
2020-12-10 18:06:34 +0000

    build based on 8d5d13d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Skewlia.jl","page":"Skewlia.jl","title":"Skewlia.jl","text":"","category":"section"},{"location":"","page":"Skewlia.jl","title":"Skewlia.jl","text":"Documentation for Skewlia.jl","category":"page"}]
+}
